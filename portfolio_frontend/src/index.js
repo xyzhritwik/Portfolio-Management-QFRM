f7d7cf7b1a60fa69ba63bf5d316190b5c3d2c3b8
xyzhritwik <56498634+xyzhritwik@users.noreply.github.com>
2025-03-25 11:19:37 +0530

    Add files via upload

---
+++ portfolio_frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PortfolioDashboard from "./PortfolioDashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <PortfolioDashboard />
+  </React.StrictMode>
+);
